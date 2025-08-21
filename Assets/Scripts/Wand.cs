4208b6ee9f30ac9cec1d65e97f9de950bcf18421
albertna <dun.na@vanderbilt.edu>
2025-08-21 11:36:00 -0500

    Bump editor version to 6.1

--- Assets/Scripts/Wand.cs
+++ Assets/Scripts/Wand.cs
@@ -5,9 +5,10 @@ using System.Runtime.InteropServices;  // Use Marshal
 using UnityEngine;
 using UnityEngine.XR;  // Use InputDevice
 
-public class Wand : MonoBehaviour {
+public class Wand : MonoBehaviour
+{
 
-//    public SteamVR_Behaviour_Pose controller;
+    //    public SteamVR_Behaviour_Pose controller;
     public bool right, left;
     public Transform tracking_space;  // Coordinate system for local hand controller positions.
     public float latency = 0f;  // Time in seconds that controller lags frame redraw.
@@ -15,44 +16,46 @@ public class Wand : MonoBehaviour {
     public Warning warning;      // For displaying warning messages.
     float last_hs = 0f;  // Last hand speed, for debugging
     float last_ac = 0f;   // Last accel magnitude, for debugging
-//    uint last_frame_index = 0;
-//    double last_frame_time = 0;
-//    ulong imu_ring_buffer = 0;  // For getting IMU acceleration values
-//    uint imu_struct_size = 0;
-    
-//    TrackedDevicePose_t[] render_poses = new TrackedDevicePose_t[OpenVR.k_unMaxTrackedDeviceCount];
-//    TrackedDevicePose_t[] game_poses = new TrackedDevicePose_t[OpenVR.k_unMaxTrackedDeviceCount];
-//    Compositor_FrameTiming timing = new Compositor_FrameTiming();
+                          //    uint last_frame_index = 0;
+                          //    double last_frame_time = 0;
+                          //    ulong imu_ring_buffer = 0;  // For getting IMU acceleration values
+                          //    uint imu_struct_size = 0;
+
+    //    TrackedDevicePose_t[] render_poses = new TrackedDevicePose_t[OpenVR.k_unMaxTrackedDeviceCount];
+    //    TrackedDevicePose_t[] game_poses = new TrackedDevicePose_t[OpenVR.k_unMaxTrackedDeviceCount];
+    //    Compositor_FrameTiming timing = new Compositor_FrameTiming();
     Vector3 prev_hv;
     Vector3 prev_pos = new Vector3();
     Quaternion prev_rot = new Quaternion();
 
     InputDevice device;
-    
-    public int device_index() {
-	// Index into pose array
-//	return controller.GetDeviceIndex();
-	return -1;
+
+    public int device_index()
+    {
+        // Index into pose array
+        //	return controller.GetDeviceIndex();
+        return -1;
     }
 
-    bool have_device() {
-	var devices = new List<InputDevice>();
-	XRNode hand = (left? XRNode.LeftHand : XRNode.RightHand);
-	InputDevices.GetDevicesAtXRNode(hand, devices);
+    bool have_device()
+    {
+        var devices = new List<InputDevice>();
+        XRNode hand = (left ? XRNode.LeftHand : XRNode.RightHand);
+        InputDevices.GetDevicesAtXRNode(hand, devices);
 
-	if(devices.Count == 1)
-	{
-	    // Debug.Log(string.Format("Device name '{0}' with role '{1}'", device.name, device.role.ToString()));
-	    device = devices[0];
-	    return true;
-	}
-	else if (devices.Count > 1)
-	{
-	    Debug.Log("Found more than one XR input device!");
-	}
-	return false;
+        if (devices.Count == 1)
+        {
+            // Debug.Log(string.Format("Device name '{0}' with role '{1}'", device.name, device.role.ToString()));
+            device = devices[0];
+            return true;
+        }
+        else if (devices.Count > 1)
+        {
+            Debug.Log("Found more than one XR input device!");
+        }
+        return false;
     }
-    
+
     // Lighthouse device serial number, e.g. "LHR-5D7C5B12"
     /*
     string serial_number() {
@@ -68,30 +71,31 @@ public class Wand : MonoBehaviour {
 	return "";
     }
     */
-    
+
     public void wand_motion(out Vector3 position, out Quaternion rotation,
         out Vector3 velocity, out Vector3 angular_velocity,
-        out Vector3 acceleration) {
+        out Vector3 acceleration)
+    {
 
-	position = velocity = angular_velocity = acceleration = Vector3.zero;
-	rotation = Quaternion.identity;
+        position = velocity = angular_velocity = acceleration = Vector3.zero;
+        rotation = Quaternion.identity;
 
-	if (!have_device())
-	    return;
+        if (!have_device())
+            return;
 
-	Vector3 p, v, a, av;
-	Quaternion r;
-	if (!device.TryGetFeatureValue(CommonUsages.devicePosition, out p))
-	    return;
-	if (!device.TryGetFeatureValue(CommonUsages.deviceRotation, out r))
-	    return;
-	if (!device.TryGetFeatureValue(CommonUsages.deviceVelocity, out v))
-	    return;
-	if (!device.TryGetFeatureValue(CommonUsages.deviceAngularVelocity, out av))
-	    return;
-	if (!device.TryGetFeatureValue(CommonUsages.deviceAcceleration, out a))
-	    return;
-	
+        Vector3 p, v, a, av;
+        Quaternion r;
+        if (!device.TryGetFeatureValue(CommonUsages.devicePosition, out p))
+            return;
+        if (!device.TryGetFeatureValue(CommonUsages.deviceRotation, out r))
+            return;
+        if (!device.TryGetFeatureValue(CommonUsages.deviceVelocity, out v))
+            return;
+        if (!device.TryGetFeatureValue(CommonUsages.deviceAngularVelocity, out av))
+            return;
+        if (!device.TryGetFeatureValue(CommonUsages.deviceAcceleration, out a))
+            return;
+
 #if old_steamvr
         int i = device_index();
         var compositor = OpenVR.Compositor;
@@ -154,46 +158,46 @@ public class Wand : MonoBehaviour {
         // Sign changes for velocity are copied form SteamVR_Controller.Device class.
         Vector3 hv = new Vector3(pose.vVelocity.v0, pose.vVelocity.v1, -pose.vVelocity.v2);
 #endif
-	
+
         Vector3 hp = p;
         Quaternion hr = r;
         // Sign changes for velocity are copied form SteamVR_Controller.Device class.
-//        Vector3 hv = new Vector3(v.x, v.y, -v.z);
-	Vector3 hv = v;
+        //        Vector3 hv = new Vector3(v.x, v.y, -v.z);
+        Vector3 hv = v;
 
-	/*
-	  TODO: Need to handle skipped frames where time step will be
-	  twice this amount.  And this wand motion routine should return
-	  the time step since the last wand motion so that the ball can
-	  be advanced by the right amount.  Advancing ball by only half
-	  the amount can cause double strikes.
-	 */
+        /*
+          TODO: Need to handle skipped frames where time step will be
+          twice this amount.  And this wand motion routine should return
+          the time step since the last wand motion so that the ball can
+          be advanced by the right amount.  Advancing ball by only half
+          the amount can cause double strikes.
+         */
         float time_step = 1.0f / 90.0f;
-	if (hv.magnitude > 2f)
-	    //hv = 2*(hp - prev_pos) / time_step - prev_hv; // Unstable
-	    hv = (hp - prev_pos) / time_step;
-//        Vector3 hav = new Vector3 (-pose.vAngularVelocity.v0, -pose.vAngularVelocity.v1, pose.vAngularVelocity.v2);
-	// Vector3 hav = new Vector3 (-av.x, -av.y, av.z);
+        if (hv.magnitude > 2f)
+            //hv = 2*(hp - prev_pos) / time_step - prev_hv; // Unstable
+            hv = (hp - prev_pos) / time_step;
+        //        Vector3 hav = new Vector3 (-pose.vAngularVelocity.v0, -pose.vAngularVelocity.v1, pose.vAngularVelocity.v2);
+        // Vector3 hav = new Vector3 (-av.x, -av.y, av.z);
 
-	// Empirical tests with Valve Index controllers show
-	// reported angular acceleration is wrong by about a 135 degree
-	// rotation about y-axis.  Oct 2, 2022.
-	Vector3 hav = Quaternion.AngleAxis(135.0f, new Vector3(0,1,0)) * av;
-	
-	if (av.magnitude > 12f && false)
-	{
-	    Quaternion rdelta = r * Quaternion.Inverse(prev_rot);
-	    float angle = 0.0f;
-	    Vector3 axis = Vector3.zero;
-	    rdelta.ToAngleAxis(out angle, out axis);
-	    float fps = 90.0f;
-	    float s = fps * angle * Mathf.PI / 180.0f;
-	    Vector3 nav = Quaternion.AngleAxis(135.0f, new Vector3(0,1,0)) * av;
-	    Debug.Log("Angular vel " + nav.x + ", " + nav.y + ", " + nav.z +
-		      " last rot " + s*axis.x + ", " + s*axis.y + ", " + s*axis.z);
-	    /* Angular velocity with Valve Index seems right for rotations around y axis (vertical), but wrong around x,z and the wrong x,z depends on paddle orientation. Ugh. */
-	}
-	prev_rot = r;
+        // Empirical tests with Valve Index controllers show
+        // reported angular acceleration is wrong by about a 135 degree
+        // rotation about y-axis.  Oct 2, 2022.
+        Vector3 hav = Quaternion.AngleAxis(135.0f, new Vector3(0, 1, 0)) * av;
+
+        if (av.magnitude > 12f && false)
+        {
+            Quaternion rdelta = r * Quaternion.Inverse(prev_rot);
+            float angle = 0.0f;
+            Vector3 axis = Vector3.zero;
+            rdelta.ToAngleAxis(out angle, out axis);
+            float fps = 90.0f;
+            float s = fps * angle * Mathf.PI / 180.0f;
+            Vector3 nav = Quaternion.AngleAxis(135.0f, new Vector3(0, 1, 0)) * av;
+            Debug.Log("Angular vel " + nav.x + ", " + nav.y + ", " + nav.z +
+                  " last rot " + s * axis.x + ", " + s * axis.y + ", " + s * axis.z);
+            /* Angular velocity with Valve Index seems right for rotations around y axis (vertical), but wrong around x,z and the wrong x,z depends on paddle orientation. Ugh. */
+        }
+        prev_rot = r;
 
         // Approximate acceleration from two velocity measurements.
         // With Samsung Odyssey Windows Mixed Reality headset and SteamVR
@@ -204,42 +208,44 @@ public class Wand : MonoBehaviour {
         Vector3 ha = (hv - prev_hv) / time_step;
         prev_hv = hv;
 
-	Vector3 vpred = (hp - prev_pos)/time_step;
-	prev_pos = hp;
-	/*
-	if (hv.magnitude > 2 && vpred.magnitude > 1.5*hv.magnitude)
-	    Debug.Log("Wand motion is larger than wand velocity for one time step " + (vpred.magnitude / hv.magnitude)
-		      + " vmove " + vpred + " vrep " + hv
-		      + " cos angle " + Vector3.Dot(vpred.normalized, hv.normalized));
-	*/
-	/*
-	Vector3 hvave = 0.5f * (hv + prev_hv);
-	if ((hvave - vpred).magnitude > 1.0f)
-	    Debug.Log("Wand motion differs from wand velocity prediction " + (vpred.magnitude / hv.magnitude)
-		      + " vmove " + vpred + " vtrack " + hvave
-		      + " cos angle " + Vector3.Dot(vpred.normalized, hvave.normalized));
-	*/
-	
+        Vector3 vpred = (hp - prev_pos) / time_step;
+        prev_pos = hp;
+        /*
+        if (hv.magnitude > 2 && vpred.magnitude > 1.5*hv.magnitude)
+            Debug.Log("Wand motion is larger than wand velocity for one time step " + (vpred.magnitude / hv.magnitude)
+                  + " vmove " + vpred + " vrep " + hv
+                  + " cos angle " + Vector3.Dot(vpred.normalized, hv.normalized));
+        */
+        /*
+        Vector3 hvave = 0.5f * (hv + prev_hv);
+        if ((hvave - vpred).magnitude > 1.0f)
+            Debug.Log("Wand motion differs from wand velocity prediction " + (vpred.magnitude / hv.magnitude)
+                  + " vmove " + vpred + " vtrack " + hvave
+                  + " cos angle " + Vector3.Dot(vpred.normalized, hvave.normalized));
+        */
+
         // Transform from tracking_space coordinates (camera) to world coordinates.
-        position = tracking_space.TransformPoint (hp);
+        position = tracking_space.TransformPoint(hp);
         rotation = tracking_space.rotation * hr;
-        velocity = tracking_space.TransformVector (hv);
-        angular_velocity = tracking_space.TransformVector (hav);
-        acceleration = tracking_space.TransformVector (ha);
+        velocity = tracking_space.TransformVector(hv);
+        angular_velocity = tracking_space.TransformVector(hav);
+        acceleration = tracking_space.TransformVector(ha);
     }
 
-    public Vector3 position() {
+    public Vector3 position()
+    {
         Vector3 p, v, av, a;
         Quaternion r;
         wand_motion(out p, out r, out v, out av, out a);
-	return p;
+        return p;
     }
 
-    public Quaternion rotation() {
+    public Quaternion rotation()
+    {
         Vector3 p, v, av, a;
         Quaternion r;
         wand_motion(out p, out r, out v, out av, out a);
-	return r;
+        return r;
     }
 
     /*
@@ -278,9 +284,10 @@ public class Wand : MonoBehaviour {
         acceleration = tracking_space.TransformVector (ha);
     }
     */
-    
+
     public void object_motion(Vector3 object_position, out Vector3 position, out Quaternion rotation,
-                                out Vector3 velocity, out Vector3 acceleration) {
+                                out Vector3 velocity, out Vector3 acceleration)
+    {
         // Object position is in hand coordinates.
         // Outputs are in world coordinates.
         Vector3 p, va;
@@ -293,28 +300,32 @@ public class Wand : MonoBehaviour {
     // Returned hand position, rotation, velocity, angular velocity and
     // acceleration are in tracking_space coordinates.
     public void predict_hand_motion(out Vector3 hp, out Quaternion hr,
-                                    out Vector3 hv, out Vector3 hav, out Vector3 ha) {
-        wand_motion (out hp, out hr, out hv, out hav, out ha);
+                                    out Vector3 hv, out Vector3 hav, out Vector3 ha)
+    {
+        wand_motion(out hp, out hr, out hv, out hav, out ha);
 
         // Predict controller position at the next frame draw time.
         // Unfortunately the Unity Oculus 1.18.1 API does not provide a way to predict
         // the pose at the next redraw, so I have to compute it myself.
-        if (latency != 0) {
+        if (latency != 0)
+        {
             // Extrapolate hand position.
             float t = latency;
             hp += t * hv + (0.5f * t * t) * ha;
             float ad = Mathf.Rad2Deg * t * hav.magnitude;
             Vector3 axis = hav.normalized;
-            hr = Quaternion.AngleAxis (ad, axis) * hr;
+            hr = Quaternion.AngleAxis(ad, axis) * hr;
             // TODO: Predict angular velocity using angular acceleration.
         }
     }
 
     // Look for hand velocity that happens when Oculus Touch controller inertial measurement
     // unit hardware exceeds 16g acceleration limit.
-    void check_imu_out_of_range(Vector3 hv, Vector3 ha) {
-        if (hv.magnitude == 0 && last_hs >= 5) {
-            warning.warn ("Acceleration out of range, last hand speed " + last_hs + " and accel " + last_ac);
+    void check_imu_out_of_range(Vector3 hv, Vector3 ha)
+    {
+        if (hv.magnitude == 0 && last_hs >= 5)
+        {
+            warning.warn("Acceleration out of range, last hand speed " + last_hs + " and accel " + last_ac);
         }
         last_hs = hv.magnitude;
         last_ac = ha.magnitude;
@@ -383,8 +394,9 @@ public class Wand : MonoBehaviour {
 	return true;
     }
     */
-    
-    public void haptic_pulse(float duration, float strength) {
+
+    public void haptic_pulse(float duration, float strength)
+    {
     }
     /*
         var dev = device ();
